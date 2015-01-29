4c9b1ab17baf0385b6dff43f31c2ff68e583c8af
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-01-29 08:36:06 +0100

    Improve namespace doc

--- zeq/types.h
+++ zeq/types.h
@@ -10,7 +10,22 @@
 #include <lunchbox/uint128_t.h>
 #include <boost/function/function1.hpp>
 
-/** Publish-subscribe classes for typed events. */
+/**
+ * Publish-subscribe classes for typed events.
+ *
+ * A Publisher opens a listening port on the network, and publishes an Event on
+ * this port. It announces its publishing URI scheme for automatic discovery.
+ *
+ * A Subscriber either explicitely subscribes to the publisher port, or uses
+ * automatic discovery to find publishers using the same URI scheme. Automatic
+ * discovery is implemented using zeroconf networking (avahi or Apple Bonjour).
+ *
+ * The connection::Broker and connection::Service may be used to introduce a
+ * subscriber to a remote, not zeroconf visible, publisher.
+ *
+ * An Event contains a strongly type, semantically defined message. Applications
+ * or groups of applications can define their own vocabulary.
+ */
 namespace zeq
 {
 
